65e1d40ee3866d8d8eee8434c813dcf323ea4c5b
ayush-that <ayush1337@hotmail.com>
2023-09-23 17:19:09 +0530

    Update

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,24 @@
-module Main (main) where
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
 
+module Main where
+
+import Data.GI.Base
 import qualified GI.Gtk as Gtk
 
 main :: IO ()
 main = do 
     Gtk.init Nothing
-    
+
+    win <- Gtk.windowNew Gtk.WindowTypeToplevel
+    Gtk.setContainerBorderWidth win 10
+    Gtk.setWindowTitle win "Alvida"
+    Gtk.setWindowResizeable win False
+    Gtk.setWindowDefaultWidth win 750
+    Gtk.setWindowDefaultHeight win 225
+    Gtk.setWindowWindowPosition win Gtk.WindowPositionCenter
+    Gtk.windowSetDecorated win False
+
+    Gtk.onWidgetDestroy win Gtk.mainQuit
+    #showAll win
+    Gtk.main
